a41b8d23dda4b884d56708fe94bcb308490aa677
piyush kumar sahu <piyushsahu42007@gmail.com>
2026-02-20 18:30:59 +0530

    20 feb 1st class of js

---
+++ Piyush-Kumar-Sahu/Javascript/index.js
@@ -0,0 +1,57 @@
+// console.log("Hello world")
+// console.log(10)
+// console.log(10.2)
+// console.log(true)
+// console.log(10-11)
+//  var x = "110" 
+//  let y = 10
+// const z = 10
+// x=10
+// y=12
+// z=11
+// var x; //declaration
+// x=10 //initialization
+// x=11 //reassignment
+//const x; //const must have a value at the time of declaration
+//const x = 10;
+// var is global scope & function scope
+// {
+//     let y=20
+//     var x = 10
+//     var x
+//     var x
+//     let y //can't redeclare 
+
+//     console.log(x)
+//     console.log(y)
+// }
+// console.log(x)
+// console.log(y)
+
+// console.log(x)
+// let x = 10;
+
+// console.log(x)
+// var x = 10; //done by me
+ 
+//var x; //done by js by is taken to line 33 so it comes as undefined and is known as "wasting"
+// x=10; //done by js
+// let x; // indefined
+
+// {
+
+//     console.log(x)
+//     let x =10
+//     console.log(x)
+//     {
+
+
+
+
+//         console.log(x)
+//         let x = 12
+//         console.log(x)
+//     }
+
+// }
+
