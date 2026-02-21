0b271cb0a3a2a7ba241e0bd77adcc320b3ea6927
Subhasmita Sahoo <subhasmita1606@gmail.com>
2026-02-21 18:19:50 +0530

    js fundamentals

--- Subhasmita-Sahoo/Javascript/index.js
+++ Subhasmita-Sahoo/Javascript/index.js
@@ -1,6 +1,33 @@
 console.log("hello world")
+  
+//age=22
+//console.log(age)
+//age="hello"
 
-//const
+//var
+//var name="saswat"
+//if(true)
+//{
+//   var age=16
+//}
+//console.log(age)
+
+//var name  step 1 declaration
+//name="saswat"  step 2 initialisation
+
+//Function
+//function getName()
+//{
+//  var name="Saswat"
+//}
+//console.log(name)
+
+//{
+//  var name="saswat"
+//}
+//console.log(name)
+
+//Const
 
 //const age=10;
 //age=60;
@@ -16,5 +43,20 @@ console.log("hello world")
 //console.log(coll)
 
 //Hoisting
-console.log(a)
-var a=10
+
+//console.log(a)
+//var a;
+//const a=10;  Reference error(cant access a before initialisation)
+//let a=10;    "   "
+
+//Temporary deadzone
+//console.log(name)
+//var name="saswat"
+//console.log(name)
+//let name="saswat"
+
+//console.log(name)
+//{
+//    console.log(name)
+//   let name='ayan'
+//}
