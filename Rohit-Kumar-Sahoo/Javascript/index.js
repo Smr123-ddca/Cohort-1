68e5df3ed7c0f6f396ca3bf2bc9dd5cb88d063ea
Rohit-kumar-sahoo <rohitkumarsahoo1606@gmail.com>
2026-02-22 15:54:20 +0530

    javascript class 2

--- Rohit-Kumar-Sahoo/Javascript/index.js
+++ Rohit-Kumar-Sahoo/Javascript/index.js
@@ -1,3 +1,5 @@
+//CLASS-1
+
 //console.log(10-5)
 //console.log("hello world")
 //console.log(10)
@@ -9,10 +11,10 @@
 
 // var x; //declaration
 // x=10 //initialization
-// x=11 //re assignment
+// x=11 //re-assignment
 
 //const x=10; //it is necessary to initialize a const
-
+//x=11 // not possible
 // var x=10;
 // console.log(x)
 // {
@@ -47,12 +49,165 @@
 
 
 
-{
-    let x =10;
-     console.log(x)
-    {
-        console.log(x)
-        let x=12;
-        console.log(x)
+// {
+//     let x =10;
+//      console.log(x)
+//     {
+//         console.log(x)
+//         let x=12;
+//         console.log(x)
+//     }
+// }
+
+// note:
+// 1. var is not used as memory leaks and it can be reassigned which is problem 
+// 2. let cannot be redeclared. so it is used. 
+// 3. const- redeclare and reinitialize both  not possible. value cannot be cahnged 
+
+
+
+
+
+
+// CLASS-2
+
+//let arr=[1,2,3,4]
+
+
+// objects- key value pair
+/*
+
+    let/const obj_name={
+        "key1":"Value1",
+        "key1":"Value1",
+        "key1":"Value1"
+        "key1":"Value1"
     }
-}
+ */     
+
+    
+// let obj= {
+//     "Student": "Rohit",
+//     "Mentor": "Pratham",
+// }
+// console.log(obj.Student)
+// console.log(obj["Student"])
+//console.log(obj["Mentor"])
+// console.log(arr[0])
+
+
+// function print(a,b){
+//     console.log(a-b)
+// }
+// print(2,3)
+// print(2,"3")
+
+// + number, string mein string is preferred 
+//- number , string mein number is preferred 
+// function print(a,b){
+//     console.log(a+b)
+// }
+// print(2,3)
+// print(2,"3")
+//print("two","2") // output is Nan
+
+
+
+
+// //Normal function
+// function print(a,b){
+//     console.log(a+b)
+// }
+// print(2,3)
+
+
+// //ARROW FUNCTIONS 
+// const fn = ()=> {
+//     console.log("Arrow")
+// }
+// fn()
+
+// //normal function can also be written in this format
+// const fn2=function(a,b){
+//     console.log(a,b)
+// }
+// fn2(2,3)
+
+// //high order function
+// function ho(a,b,cb){ // cb is callback function
+//     console.log(a,b,cb)
+    
+// }
+// ho(2,3,print)
+
+// //high order function calling the print function 
+// function ho(a,b,cb){ // cb is callback function
+//     cb(a,b)
+// }
+// ho(2,3,print)
+
+
+
+//javascript works on single thread.
+
+// context switching and synchronous
+//  console.log("Start")
+//  setTimeout(()=>{
+//     console.log("My Process")
+//  },2000)
+//  console.log("end")
+//output
+// start
+// end
+// Myprocess
+
+// setTimeout(fn,time in ms)
+
+
+
+//await can only be used with asyncgronous . without await , it will work similarly like synchronous 
+// async function asy(){
+//     console.log("Start")
+//     const data=await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     console.log(data)
+//     console.log("end")
+// }
+// asy()
+
+
+//both are same 
+//const val=function(){}
+//const val2 = ()=> {}
+
+
+// async function asy(){
+//     console.log("Start")
+//     const data=await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then((val)=>{ //resolve
+//         console.log("succes")
+//         console.log(val)
+//     })
+//     .catch((err)=>{ //reject
+//         console.log("failed")
+//         console.log(err)
+//     })
+//     .finally(()=>{
+//         console.log("process completed")
+//     })
+//     console.log(data)
+//     console.log("end")
+// }
+// asy()
+
+
+
+// const arrow =(...args)=>{
+//     console.log(args)
+// }
+// function name(){
+//     console.log(arguments)
+// }
+// name(1,"Hello",3)
+// arrow("arrow 1", "arrow 2")
+// const arr=[2,3,4]
+// console.log(...arr)
