b3cd496ee753fc57642849fcfa5792730d2c380e
AdityaSekharDas <adityasekhardas09@gmail.com>
2026-02-22 15:57:06 +0530

    Sunday class

--- Aditya-Sekhar-Das/Javascript/index.js
+++ Aditya-Sekhar-Das/Javascript/index.js
@@ -7,14 +7,36 @@ console.log(name);
 const age = 18;
 console.log(age);
 
-{
-    const age1 = 20;
-    const name = "Aditya Sekhar Das";
-    console.log(name);
+
+
+let arr = [1,2,3,4,5];
+console.log(arr);
+console.log(arr[2]);
+
+let obj = {
+    "name" : "aditya",
+    "age" : 18
 }
+console.log(obj);
+console.log(obj["name"]);
+console.log(obj.age);
 
-console.log(age1);
+function print(a,b) {
+    console.log(a+b);
+    console.log(a-b);
+}
+print(2,"3");
+print("3",2);
 
+const func = function(a,b) {
+    console.log(a+b);
+}
+func(2,2);
+
+const fn = (a,b) => {
+    console.log(a+b);
+}
+fn(3,8);
 
 
 
