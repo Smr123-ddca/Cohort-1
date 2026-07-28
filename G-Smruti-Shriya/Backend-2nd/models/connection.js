70fce60db997448488ab611564591adad68aa786
G Smruti Shriya <gsmrutishriya12@gmail.com>
2026-07-28 21:57:25 +0530

    Backend-2nd

---
+++ G-Smruti-Shriya/Backend-2nd/models/connection.js
@@ -0,0 +1,16 @@
+const {client,query,Pool} = require('pg')
+
+const pool = new Pool({
+    idleTimeoutMillis:30000,
+})
+
+pool.connect((err,client,release) => {
+    if(err) console.log(err)
+    console.log("Succesfully Connected database");
+    release();
+})
+
+module.exports = {
+    client : ()=> pool.connect(),
+    query :(text,params)=> pool.query(text,params)
+}
---
+++ G-Smruti-Shriya/Backend-2nd/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+require('dotenv').config()
+const {initDatabase} = require('./controllers/initDb.js');
+const db = require('./models/connection.js');
+initDatabase();
+
+PORT = process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        status:"Success",
+        message: "Welcome to the home page"
+    })
+})
+
+app.get('/users', async (req,res) => {
+    const getUsersQuery = `
+    SELECT * FROM users;
+    `
+    try{
+        const result = await db.query(getUsersQuery);
+        res.status(200).json({
+            status:"Success",
+            message : "All users Fetched",
+            data : result,rows
+        })
+    }catch(error){
+        return res.send(500).json({
+            status:"Failed",
+            message : "Something went wrong",
+            error : error
+        })
+    }
+    
+})
+
+app.listen(PORT , (err)=>{
+    if(err) console.log(err);
+    console.log('Successfully connected to server at port ${PORT}');
+})
